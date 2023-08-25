33aae7351a606dbdf208453edc49009883edb84e
phongismeok <130113683+phongismeok@users.noreply.github.com>
2023-08-25 19:16:50 +0700

    phong

---
+++ app/src/main/java/com/example/quanlythoigian/feedback.java
@@ -0,0 +1,59 @@
+package com.example.quanlythoigian;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import fragment.frgchat;
+import fragment.frggroup;
+import fragment.frghome;
+import fragment.frgperson;
+
+public class feedback extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        BottomNavigationView bottomnav;
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+
+        bottomnav = findViewById(R.id.bottomnav);
+
+        bottomnav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId()== R.id.home){
+                    frghome frghome = new frghome();
+                    repalceFrg(frghome);
+                }
+                else if(item.getItemId()==R.id.chat){
+                    frgchat frgchat = new frgchat();
+                    repalceFrg(frgchat);
+                }
+                else if(item.getItemId()==R.id.group){
+                    frggroup frggroup = new frggroup();
+                    repalceFrg(frggroup);
+                }
+                else if(item.getItemId()==R.id.person){
+                    frgperson frgperson = new frgperson();
+                    repalceFrg(frgperson);
+                }
+                return true;
+            }
+        });
+
+    }
+    public void repalceFrg(Fragment frg){
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().replace(R.id.frmfeedback,frg).commit();
+    }
+}
