2837c8c734ee418e0a7251f6fcd68a64329e4ac0
Luat dep trai <dangluat02@gmail.com>
2023-09-01 20:44:48 +0700

    dk dk file java1

---
+++ app/src/main/java/com/example/quanlythoigian/TrangChu.java
@@ -0,0 +1,14 @@
+package com.example.quanlythoigian;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TrangChu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trang_chu);
+    }
+}
