a124d2326c6992b93e0f30f21d97d50f8d5db0f4
phongismeok <130113683+phongismeok@users.noreply.github.com>
2023-09-01 20:42:25 +0700

    phong

--- app/src/main/java/com/example/quanlythoigian/feedback.java
+++ app/src/main/java/com/example/quanlythoigian/feedback.java
@@ -12,7 +12,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 import fragment.frgchat;
-import fragment.frggroup;
+import fragment.frggroup3;
 import fragment.frghome;
 import fragment.frgperson;
 
@@ -26,27 +26,27 @@ public class feedback extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
 
-        bottomnav = findViewById(R.id.bottomnav1);
+        bottomnav = findViewById(R.id.bottomnav);
 
         bottomnav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//                if(item.getItemId()== R.id.home){
-//                    frghome frghome = new frghome();
-//                    repalceFrg(frghome);
-//                }
-//                else if(item.getItemId()==R.id.chat){
-//                    frgchat frgchat = new frgchat();
-//                    repalceFrg(frgchat);
-//                }
-//                else if(item.getItemId()==R.id.group){
-//                    frggroup frggroup = new frggroup();
-//                    repalceFrg(frggroup);
-//                }
-//                else if(item.getItemId()==R.id.person){
-//                    frgperson frgperson = new frgperson();
-//                    repalceFrg(frgperson);
-//                }
+                if(item.getItemId()== R.id.home){
+                     frghome frghome = new frghome();
+                    repalceFrg(frghome);
+                }
+                else if(item.getItemId()==R.id.chat){
+                    frgchat frgchat = new frgchat();
+                    repalceFrg(frgchat);
+                }
+                else if(item.getItemId()==R.id.group){
+                    frggroup3 frggroup = new frggroup3();
+                    repalceFrg(frggroup);
+                }
+                else if(item.getItemId()==R.id.person){
+                    frgperson frgperson = new frgperson();
+                    repalceFrg(frgperson);
+                }
                 return true;
             }
         });
